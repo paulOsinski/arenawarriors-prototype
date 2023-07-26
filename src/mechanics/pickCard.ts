879f1234f27b85a5b045cce3c0ce711b7509992a
Paul Osinski <42211303+paulOsinski@users.noreply.github.com>
2023-07-26 15:51:57 -0400

    add description pickCard.ts

--- src/mechanics/pickCard.ts
+++ src/mechanics/pickCard.ts
@@ -1,4 +1,4 @@
-// Picks a card, returning the card name and the card's pip values
+// Picks a card from an index, returning the card name and the card's pip values.  Used in fullCardTest.
 
 export const pickCard = (a: number, b: object) => {
   const name = Object.keys(b)[a];
